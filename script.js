966d19a89c992144dff13aa4bb152dba0f0f0b31
user.name: "techaffairs4@gmail.com <techaffairs4@gmail.com>
2024-06-15 21:06:37 +0530

    random quote using math.random from array of quotes

--- script.js
+++ script.js
@@ -2,34 +2,51 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+let loading = document.getElementById('loader');
+let quoteBox = document.getElementById('quote-box');
 
-async function newQuote(){
+//show Loader
+function loader() {
+    loading.hidden = false;
+    quoteBox.hidden = true;
+}
+//hide loader
+function complete() {
+    if (!loader.hidden) {
+        loading.hidden = true;
+        quoteBox.hidden = false;
+    }
+}
+async function newQuote() {
     
-    const apiUrl = 'https://dummyjson.com/quotes/random';
-    try{
+    const apiUrl = 'https://dummyjson.com/quotes';
+    try {
+        complete();
         const response = await fetch(apiUrl);
         let data = await response.json();
         console.log(data);
 
-        authorText.innerText = data.author;
-        quoteText.innerText = data.quote;
-    }catch(error){
-     console.log('error detected in api call');
-    //  process.exit(1);
+        let random = Math.floor(Math.random() * data.quotes.length);
+        console.log(random);
+        authorText.innerText = data.quotes[random].author;
+        quoteText.innerText = data.quotes[random].quote;
+        loader();
+    } catch (error) {
+        console.log('error detected in api call');
+        newQuote();
+        //  process.exit(1);
     }
 }
 newQuote();
-
 //Tweet Quote
-function tweetQuote(){
+function tweetQuote() {
     const quote = quoteText.innerText;
     const author = authorText.innerText;
-
-    const twitterUrl = 'https://twitter.com/intent/tweet?text=${quote} - ${author}';
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote}-${author}`;
     window.open(twitterUrl, '_blank');
 }
 
-newQuoteBtn.addEventListener('click',newQuote);
-twitterBtn.addEventListener('click',tweetQuote);
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 
